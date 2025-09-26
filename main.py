826651f34b027a3158b15f6ec9dc16cd047153d1
jasonsoprovich <190052169+jasonsoprovich@users.noreply.github.com>
2025-09-25 22:40:22 -0600

    feat: add char count sort and report output

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
-from stats import get_word_count
-from stats import get_char_count
+from stats import get_word_count, get_sorted_chars
 
 def main():
-  word_count = get_word_count()
-  char_count = get_char_count()
-  print(f"Found {word_count} total words")
-  print(char_count)
+  print("============ BOOKBOT ============")
+  print("Analyzing book found at books/frankenstein.txt...")
+  print("----------- Word Count ----------")
+  print(f"Found {get_word_count()} total words")
+  print("--------- Character Count -------")
+  sorted_chars = get_sorted_chars()
+  for char_dict in sorted_chars:
+    print(f"{char_dict["char"]}: {char_dict["num"]}")
+  print("============= END ===============")
 
 main()
--- stats.py
+++ stats.py
@@ -11,5 +11,17 @@ def get_char_count():
   char_list = list(get_book_text().lower())
   char_count = {}
   for char in char_list:
-    char_count[char] = char_count.get(char, 0) + 1
-  return(char_count)
+    if char.isalpha():
+      char_count[char] = char_count.get(char, 0) + 1
+  return char_count
+
+def sort_on(dict):
+  return dict["num"]
+
+def get_sorted_chars():
+  char_count = get_char_count()
+  sorted_chars = []
+  for char, count in char_count.items():
+    sorted_chars.append({"char": char, "num": count})
+  sorted_chars.sort(key=sort_on, reverse=True)
+  return sorted_chars
